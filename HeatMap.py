7842b8762957404c51129c3f99a902988e68929f
Martin J. R. Jensen <mjrj38934@edu.ucl.dk>
2021-09-17 11:12:14 +0200

    Add project files.

---
+++ HeatMap.py
@@ -0,0 +1,24 @@
+﻿from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="HeatMap")
+
+file = open('Addresses.txt', 'r', encoding="UTF-8")
+addresses = file.readlines()
+
+latitudes = [0] * len(addresses)
+longitudes = [0] * len(addresses)
+
+if len(addresses) > 0:
+	for idx, x in enumerate(addresses):
+		location = geolocator.geocode(x)
+		latitudes[idx] = location.latitude
+		longitudes[idx] = location.longitude
+
+	print("Latitudes:")
+	for x in latitudes:
+		print(x)
+
+	print("\nLongtitudes:")
+	for x in longitudes:
+		print(x)
+else:
+	print("No addresses found.")
