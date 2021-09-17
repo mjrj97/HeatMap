97cc16117eaf7dde438674286e98896269bbb3e5
mjrj97 <martin.jensen.1997@hotmail.com>
2021-09-17 14:40:56 +0200

    Can now put people in as heatmap and addresses as dots.

--- HeatMap.py
+++ HeatMap.py
@@ -1,25 +1,63 @@
 ﻿import gmplot
+import os.path
+from os import path
 from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="HeatMap")
 
-file = open('Addresses.txt', 'r', encoding="UTF-8")
-addresses = file.readlines()
+peopleExist = path.exists("People.txt")
+addressesExist = path.exists("Addresses.txt")
 
-latitudes = [0] * len(addresses)
-longitudes = [0] * len(addresses)
+if peopleExist:
+	peopleFile = open('People.txt', 'r', encoding="UTF-8")
+	people = peopleFile.readlines()
+	print("Found People.txt")
 
-if len(addresses) > 0:
-	for idx, x in enumerate(addresses):
-		location = geolocator.geocode(x)
-		if location is None:
-			print("Error: " + addresses[idx], end='')
-		else:
-			print("Loaded: " + addresses[idx], end='')
-			latitudes[idx] = location.latitude
-			longitudes[idx] = location.longitude
-
-	gmap = gmplot.GoogleMapPlotter(latitudes[0], longitudes[0], 13)
-	gmap.heatmap(latitudes, longitudes, radius=75, opacity=0.3, max_intensity=5, dissipating = False)
-	gmap.draw("C:\\Users\\marti\\Desktop\\map.html")
+	pLatitudes = [0] * len(people)
+	pLongitudes = [0] * len(people)
 else:
-	print("No addresses found.")
+	print("Error: Could not find People.txt in folder")
+
+if addressesExist:
+	addressesFile = open('Addresses.txt', 'r', encoding="UTF-8")
+	addresses = addressesFile.readlines()
+	print("Found Addresses.txt")
+
+	aLatitudes = [0] * len(addresses)
+	aLongitudes = [0] * len(addresses)
+else:
+	print("Error: Could not find Addresses.txt in folder")
+
+if peopleExist & addressesExist:
+	print(len(people))
+	print(len(addresses))
+	if len(people) > 0:
+		geolocator = Nominatim(user_agent="HeatMap")
+
+		print("Loading people into map...")
+		for idx, x in enumerate(people):
+			location = geolocator.geocode(x)
+			if location is None:
+				print("Error: " + people[idx], end='')
+			else:
+				print("Loaded: " + people[idx], end='')
+				pLatitudes[idx] = location.latitude
+				pLongitudes[idx] = location.longitude
+
+		print("Loading addresses into map...")
+		for idx, x in enumerate(addresses):
+			location = geolocator.geocode(x)
+			if location is None:
+				print("Error: " + addresses[idx], end='')
+			else:
+				print("Loaded: " + addresses[idx], end='')
+				aLatitudes[idx] = location.latitude
+				aLongitudes[idx] = location.longitude
+
+		gmap = gmplot.GoogleMapPlotter(pLatitudes[0], pLongitudes[0], 13)
+		gmap.heatmap(pLatitudes, pLongitudes, radius=75, opacity=0.3, max_intensity=5, dissipating = False)
+		gmap.scatter(aLatitudes, aLongitudes, c='#3B0B39', size = 40, marker = False )
+		gmap.draw("C:\\Users\\marti\\Desktop\\map.html")
+	else:
+		if len(people) == 0:
+			print("No people in People.txt")
+		if len(addresses) == 0:
+			print("No addresses in Address.txt")
